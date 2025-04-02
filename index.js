a75a55854884b9978df7f940c578384b863a814c
epix play <epixplayenikop@gmail.com>
2025-04-02 13:32:44 -0500

    Fix: definir addonInterface correctamente

--- index.js
+++ index.js
@@ -158,7 +158,10 @@ builder.defineStreamHandler(async ({ id }) => {
 
 const PORT = process.env.PORT || 7010;
 const HOST = '0.0.0.0';
+const addonInterface = builder.getInterface();
 
-require("http").createServer(serveHTTP(addonInterface)).listen(PORT, HOST, () => {
-  console.log(`✅ Addon corriendo en: http://${HOST}:${PORT}/manifest.json`);
+require("http")
+  .createServer(serveHTTP(addonInterface))
+  .listen(PORT, HOST, () => {
+    console.log(`✅ Addon corriendo en: http://${HOST}:${PORT}/manifest.json`);
 });
